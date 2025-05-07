8ad68036310666a582d8a897f8c64e55fe97b7a0
Dev-Abhijeet <abhijeetkamble14092000@gmail.com>
2025-05-07 17:49:37 +0530

    A script that collect logs from server files and store it into database - added comments to it

--- system_log_analyser.sh
+++ system_log_analyser.sh
@@ -3,25 +3,32 @@
 # Author : Abhijeet K
 # Reason : Daily Log monitoring and keeping records
 
-# Get Server IP
+# Defining database credentilas 
 DB_HOST="10.251.251.5" 
 DB_DB="servers"
 DB_TABLE="server_logs"
 DB_USER="root"
 DB_PASSWORD="abhi1407"
 
+#Extracting IP & Hostname of server and storing into varibles
 ip_add=$(hostname -I | awk '{print $1}')
 host=$(hostname)
+
+#Defining Log file in which error logs will be redirected
 log_store="/tmp/logs.log"
 
+#Common default server log files 
 FILES=("/var/log/messages" "/var/log/dmesg" "/var/log/syslog" "/var/log/kern.log" "/var/log/dpkg.log")
 
+#Defining some keywords to get collect logs 
 keyword=("error" "oom" "out of memory" "fail" "fatal" "timeout")
 
+#Checking file exist or not
 if [[ ! -f "$log_store" ]];then
 	touch $log_store
 fi
 
+#Checking logs in files as per keywords and storing it into file
 for file in "${FILES[@]}"
 do
 	if [[ -f "$file" ]];
@@ -35,9 +42,10 @@ do
 	fi
 done	
 
-#Fetching data
+#storing log file data into new variable
 data=$(cat $log_store)
 
+#Checking if DB is exist or not and not present then creating it 
 check_db=$(mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -e "show databases" | grep -i "$DB_DB")
 
 if [[ -n "$check_db" ]];then
@@ -47,8 +55,8 @@ else
 
 fi
 
-echo "$check_db"
 
+#Checking if Table is exist or not and if not then creating it
 check_table=$(mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_DB" -e "show tables" | grep -i "$DB_TABLE")
 
 if [[ -n "$check_table" ]];then
@@ -62,5 +70,6 @@ else
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
 fi
 
+#Inserting Data into table 
 mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_DB" -e "INSERT INTO $DB_TABLE (ip_address, hostname, log_data) VALUES ('$ip_add', '$host', '$data');"
 
